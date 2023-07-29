5485fe147a04d627830da99198f3c4b3f0f167e8
Ayotunde Obasa <55897771+ayotunde-codes@users.noreply.github.com>
2023-07-30 00:45:02 +0100

    auto

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,6 +1,6 @@
+import Header from "./Header"
 import React from "react"
 import SideNav from "./SideNav"
-import Header from "./Header"
 
 type ILayoutProps = {
   children: React.ReactNode
@@ -8,7 +8,7 @@ type ILayoutProps = {
 
 export default function Layout({ children }: ILayoutProps) {
   return (
-    <div className="grid w-full grid-flow-col">
+    <div className="grid w-full grid-flow-col  grid-cols-[max-content_auto]">
       <SideNav />
       <div className="flex min-h-screen flex-col items-center justify-between w-full">
         <Header />
